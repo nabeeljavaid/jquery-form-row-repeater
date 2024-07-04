dbd2125382b3827f2d452edafacebe2eb45d2e2e
Nabeel Javaid <nabeeljavaid.nmj@gmail.com>
2024-07-04 22:04:30 +0500

    Improve Core Functionality to Support Nested Row Repeaters

--- jquery-form-row-repeater.js
+++ jquery-form-row-repeater.js
@@ -8,41 +8,56 @@
             rowClass: '.form-row',
             handleClass: '.handle',
             removeButtonClass: '.remove-row',
-            sortOrderClass: '.sort-order'
+            sortOrderClass: '.sort-order',
+            afterAddedRow: null // Callback function
         }, options);
 
         // Function to update sort order
-        function updateSortOrder() {
-            $(settings.sortableContainer).find(settings.rowClass).each(function(index) {
+        this.updateSortOrder = function() {
+            console.log('sort ordering....');
+            console.log('sortableContainer: ' + settings.sortableContainer);
+            console.log('rowClass: ' + settings.rowClass);
+            console.log('sortOrderClass: ' + settings.sortOrderClass);
+            $(settings.sortableContainer, this).find(settings.rowClass).each(function(index) {
                 $(this).find(settings.sortOrderClass).val(index + 1);
             });
         }
 
         // Clear values of a form row
         function clearRowValues($row) {
+            console.log('clearing form....');
+            //console.log('sortOrderClass: ' + settings.sortOrderClass);
             $row.find('input[type="text"]').val('');
             $row.find('select').val('');
             $row.find('input[type="checkbox"], input[type="radio"]').prop('checked', false);
         }
 	
 	// Add a new row
-        this.addRow = function() {
+        this.addRow = function(event) {
+            console.log('adding row....');
             const $templateRow = $(settings.templateRow);
             const $newRow = $templateRow.first().clone();
             clearRowValues($newRow); // Clear input values
-            const rowCount = $(settings.sortableContainer).children(settings.rowClass).length;
+            const rowCount = $(settings.sortableContainer, this).children(settings.rowClass).length;
             $newRow.find(settings.sortOrderClass).val(rowCount + 1);
-            $(settings.sortableContainer).append($newRow);
-            updateSortOrder();
+            $(settings.sortableContainer, this).append($newRow);
+            this.updateSortOrder();
+            
+            // Call the afterAddedRow callback if provided
+            if (typeof settings.afterAddedRow === 'function') {
+                settings.afterAddedRow($newRow);
+            }
+            
         };
 
         // Initialize sortable functionality
         this.makeSortable = function() {
-            $(settings.sortableContainer).sortable({
+            const self = this; // Capture `this` context
+            $(settings.sortableContainer, this).sortable({
                 handle: settings.handleClass,
                 axis: 'y',
                 update: function(event, ui) {
-                    updateSortOrder();
+                    self.updateSortOrder();
                 }
             });
         };
@@ -50,21 +65,25 @@
         // Initialize the plugin
         this.init = function() {
             console.log('Initialize the plugin');
+            console.log('ID: ' + this.attr('id'));
             this.makeSortable();
-            $(settings.addRowButton).on('click', () => {
+            $(settings.addRowButton, this).on('click', (event) => {
+            	 event.preventDefault();
                 this.addRow();
                 this.makeSortable();
             });
-            $(settings.sortableContainer).on('click', settings.removeButtonClass, function() {
+            $(settings.sortableContainer, this).on('click', settings.removeButtonClass, function() {
                 $(this).closest(settings.rowClass).remove();
-                updateSortOrder();
+                this.updateSortOrder();
             });
 
             // Show the first row by default
-            $(settings.sortableContainer).children(settings.rowClass).first();
+            $(settings.sortableContainer, this).children(settings.rowClass).first();
 
             // Update sort order initially
-            updateSortOrder();
+            this.updateSortOrder();
+            
+            
         };
 
         return this.init();
