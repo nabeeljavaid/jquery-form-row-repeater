adc8abffbe7f22859a41414f644f2c63a69d0be1
Nabeel Javaid <nabeeljavaid.nmj@gmail.com>
2024-07-05 19:48:47 +0500

    Advanced Functionality (WORKING)

--- jquery.formRowRepeater.js
+++ jquery.formRowRepeater.js
@@ -18,11 +18,11 @@
  *   @param {string} [options.sortOrderClass='.sort-order'] - Class for sorting order input field.
  *   @param {function} [options.afterAddedRow=null] - Callback function after adding a row.
  */
- 
+
 (function($) {
 	// Define the plugin
 	$.fn.formRowRepeater = function(options) {
-	
+
 		const settings = $.extend({
 			templateRow: '.template-row',
 			addRowButton: '#add-row',
@@ -31,9 +31,10 @@
 			handleClass: '.handle',
 			removeButtonClass: '.remove-row',
 			sortOrderClass: '.sort-order',
+			rowCounterPlaceholder: '{n}',
 			afterAddedRow: null // Callback function
 		}, options);
-		
+
 		let $template = null;
 		let $rowCount = 0;
 
@@ -52,28 +53,39 @@
 			$row.find('select').val('');
 			$row.find('input[type="checkbox"], input[type="radio"]').prop('checked', false);
 		}
-
+		
+		function generateUUID() {
+		  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		    var r = Math.random() * 16 | 0,
+			v = c === 'x' ? r : (r & 0x3 | 0x8);
+		    return v.toString(16);
+		  });
+		}
+		
 		// Add a new row
-		this.addRow = function(event) {
+		this.addRow = function($rowCounter) {
 			console.log('adding row....');
 			
-			$rowCount++;
+			console.log('rowCounter:' + $rowCounter);
+			const $rowId = generateUUID();
 			let $newRow = $template.clone();
-			$newRow.find('input, select').each(function() {
-			    const name = $(this).attr('name').replace('{n}', $rowCount);
-			    $(this).attr('name', name);
-			});
+			$newRow.attr('data-row-id', $rowId); // Set UUID attribute
 			
+			$newRow.find('input, select').each(function() {
+				const name = $(this).attr('name').replace(settings.rowCounterPlaceholder, $rowCounter);
+				$(this).attr('name', name);
+			});
+
 			clearRowValues($newRow); // Clear input values
 			const rowCount = $(settings.sortableContainer, this).children(settings.rowClass).length;
 			$newRow.find(settings.sortOrderClass).val(rowCount + 1);
 			$(settings.sortableContainer, this).append($newRow);
-			
+
 			this.updateSortOrder();
 
 			// Call the afterAddedRow callback if provided
 			if (typeof settings.afterAddedRow === 'function') {
-				settings.afterAddedRow($newRow);
+				settings.afterAddedRow($rowId);
 			}
 
 		};
@@ -93,34 +105,31 @@
 		// Initialize the plugin
 		this.init = function() {
 			console.log('Initialize the plugin');
-			
-			$template = $(settings.templateRow).first().clone();
-		      	$template.removeClass('template-row'); // Remove template class
 
-		      	// Remove the original template row from DOM
-		      	$(settings.templateRow).first().remove();
-      			
-      			// Append a first row on initialization
-      			this.addRow();
+			$template = $(settings.templateRow).first().clone();
 			
+			
+			// Remove the original template row from DOM
+			$(settings.templateRow, this).first().remove();
+
+			// Append a first row on initialization
+			this.addRow(0);
+
 			this.makeSortable();
-			
+
 			// Add row event handler
 			$(settings.addRowButton, this).on('click', (event) => {
 				event.preventDefault();
-				this.addRow();
+				$rowCount++;
+				this.addRow($rowCount);
 				this.makeSortable();
 			});
-			
+
 			// Remove row event handler
 			$(settings.sortableContainer, this).on('click', settings.removeButtonClass, function() {
-			        console.log('removing row....');
+				console.log('removing row....');
 				$(this).closest(settings.rowClass).remove();
-				updateSortOrder();
 			});
-
-			// Show the first row by default
-			// $(settings.sortableContainer, this).children(settings.rowClass).first();
 
 			// Update sort order initially
 			this.updateSortOrder();
